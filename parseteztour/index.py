53791bc8ba020ec2ccabc4aaa3ddee6069c0132b
Ismail Gadzhiev <ismailgadzhievff@gmail.com>
2023-07-26 16:32:57 +0200

    create template for parsing

--- parseteztour/index.py
+++ parseteztour/index.py
@@ -3,53 +3,54 @@ import ydb
 import os
 import json
 
-boto_session = boto3.session.Session(
-    aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
-    aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"]
-)
-
-s3 = boto_session.client(
-    service_name='s3',
-    endpoint_url='https://storage.yandexcloud.net',
-    region_name='ru-central1'
-)
-
-# Create driver in global space.
-driver = ydb.Driver(
-    endpoint=os.getenv('YDB_ENDPOINT'), database=os.getenv('YDB_DATABASE'),
-    credentials=ydb.iam.MetadataUrlCredentials(),)
-# Wait for the driver to become active for requests.
-driver.wait(fail_fast=True, timeout=5)
-# Create the session pool instance to manage YDB sessions.
-pool = ydb.SessionPool(driver)
-
-from typing import Optional, Callable
+from typing import Optional, Callable, List
 from bs4 import BeautifulSoup
 
 
-def get_text(
-    soup: BeautifulSoup,
-    element: str,
-    class_: str,
-    raise_error: bool = True,
-    attrs: Optional[list] = None,
-) -> str:
-    card = soup.find(element, class_=class_)
+def get_s3_client():
+    boto_session = boto3.session.Session(
+        aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+    )
 
-    if card is None and raise_error:
-        raise ValueError(f"Element {element} with class {class_} not found")
-    elif card is None:
-        if attrs is None:
-            return None
-        else:
-            return None, *(None for _ in range(len(attrs)))
-    else:
-        text = card.get_text(" ", strip=True)
-        if attrs is None:
-            return text
-        else:
-            attr_value = (card.attrs[a] for a in attrs)
-            return text, *attr_value
+    client = boto_session.client(
+        service_name="s3",
+        endpoint_url="https://storage.yandexcloud.net",
+        region_name="ru-central1",
+    )
+    return client
+
+
+def initialize_session():
+    driver = ydb.Driver(
+        endpoint=os.getenv("YDB_ENDPOINT"),
+        database=os.getenv("YDB_DATABASE"),
+        credentials=ydb.iam.MetadataUrlCredentials(),
+    )
+
+    try:
+        driver.wait(fail_fast=True, timeout=5)
+        session = ydb.SessionPool(driver)
+        return session
+    except TimeoutError:
+        print("Connect failed to YDB")
+        print("Last reported errors by discovery:")
+        print(driver.discovery_debug_details())
+        exit(1)
+
+
+def create_execute_query(query):
+    # Create the transaction and execute query.
+    def _execute_query(session):
+        session.transaction().execute(
+            query,
+            commit_tx=True,
+            settings=ydb.BaseRequestSettings()
+            .with_timeout(3)
+            .with_operation_timeout(2),
+        )
+
+    return _execute_query
 
 
 from functools import wraps
@@ -75,34 +76,227 @@ def parse_func_wrapper(website: str, time_fmt: str = "%Y-%m-%dT%H:%M:%SZ") -> Ca
 
     return dec_outer
 
+
 import os
-from typing import List
+
 
 def update_dicts(dicts: List[dict], **kwargs) -> List[dict]:
     return list(map(lambda x: {**x, **kwargs}, dicts))
 
+
+def load_process_html_cards_from_s3(
+    client, Bucket: str, Key: str, get_cards: Callable, parse_card: Callable
+) -> list:
+    prefix = "/".join(Key.split("/")[:-1])
+    object_key = os.path.join(prefix, "content.html")
+    meta_key = os.path.join(prefix, "meta.json")
+
+    meta_object_response = client.get_object(Bucket=Bucket, Key=meta_key)
+
+    meta = json.loads(meta_object_response["Body"].read())
+
+    if not meta["failed"]:
+        get_object_response = client.get_object(Bucket=Bucket, Key=object_key)
+        content = get_object_response["Body"].read()
+        soup = BeautifulSoup(content, "html.parser")
+        cards = get_cards(soup)
+        result = list(map(parse_card, cards))
+        result_with_meta = update_dicts(
+            result, parsing_id=meta["parsing_id"], key=Key, bucket=Bucket
+        )
+
+        # client.delete_object(Bucket=Bucket, Key=Key)
+        # client.delete_object(Bucket=Bucket, Key=object_key)
+        # client.delete_object(Bucket=Bucket, Key=meta_key)
+        return result_with_meta
+    else:
+        # client.delete_object(Bucket=Bucket, Key=Key)
+        # client.delete_object(Bucket=Bucket, Key=meta_key)
+        return None
+
+
+BASE_FMT = (
+    ',cast("{created_dttm}" as datetime), "{website}",'
+    '"{link}", "{offer_hash}", "{row_id}", "{parsing_id}",'
+    '"{key}", "{bucket}"'
+)
+
+QUERY_BULK_INSERT = """REPLACE INTO `{table}` (
+    {fields})
+VALUES
+{rows}"""
+
+
+class RawUtf8Table:
+    table_name = "raw/table"
+    default_fields = [
+        "created_dttm",
+        "website",
+        "link",
+        "offer_hash",
+        "row_id",
+        "parsing_id",
+        "key",
+        "bucket",
+    ]
+
+    custom_fields = []
+
+    def __init__(
+        self, base_dir: str, session: ydb.SessionPool, max_records: int = 5
+    ) -> None:
+        self.session = session
+        self.base_dir = base_dir
+        self.path = os.path.join(base_dir, self.table_name)
+        self.fields =  self.custom_fields + self.default_fields
+        self.fields_str = ",".join(self.fields)
+        custom_format_string = ",".join(
+            map(lambda x: '"{' + x + '}"', self.custom_fields)
+        )
+        self.format_string_row = custom_format_string + BASE_FMT
+        self.max_records = max_records
+
+    def _format_record(self, record: dict):
+        return "(" + self.format_string_row.format(**record) + ")"
+
+    def _create_queries(self, data: List[dict]):
+        queries = []
+        for i in range(len(data) // self.max_records + 1):
+            sub_data = data[i * self.max_records : (i + 1) * self.max_records]
+            if len(sub_data) > 0:
+                rows = data[i * self.max_records : (i + 1) * self.max_records]
+                rows_str = ",".join(map(self._format_record, rows))
+                queries.append(
+                    QUERY_BULK_INSERT.format(
+                        table=self.path, fields=self.fields_str, rows=rows_str
+                    )
+                )
+        return queries
+
+    def bulk_insert(self, data: List[dict]):
+        queries = self._create_queries(data)
+
+        for query in queries:
+            try:
+                self.session.retry_operation_sync(create_execute_query(query))
+            except Exception as e:
+                raise Exception(f"Failed at query {query[:150]}\nwith exception {e}")
+
+
+# Helpful utility, allows to extract text from html
+
+
+def get_text(
+    soup: BeautifulSoup,
+    element: str,
+    class_: str,
+    raise_error: bool = True,
+    attrs: Optional[list] = None,
+) -> str:
+    """Helpful utility, allows to extract text from soup html
+    Params:
+    soup: BeautifulSoup
+        soup element from which text is extracted
+    element: str
+        name of an element, from which text should be extracted, i.e. div, a, i etc
+    class_: str
+        html class of an element, from which text should be extracted
+    raise_error: bool
+        whether to raise error if element is not found
+    attrs: list of string
+        if passed, function will also return these attributes parsed from element
+    """
+
+    card = soup.find(element, class_=class_)
+
+    if card is None and raise_error:
+        raise ValueError(f"Element {element} with class {class_} not found")
+    elif card is None:
+        if attrs is None:
+            return None
+        else:
+            return None, *(None for _ in range(len(attrs)))
+    else:
+        text = card.get_text(" ", strip=True)
+        if attrs is None:
+            return text
+        else:
+            attr_value = (card.attrs[a] for a in attrs)
+            return text, *attr_value
+
+
+# ----------------
+# Change from here
+# ----------------
+
+
+class RawTeztour(RawUtf8Table):
+    """Raw table class
+
+    To create your table, just change the name of the class,
+    attribute table_name and add your custom fields
+    all those fields should be string!
+
+    """
+
+    table_name = "raw/teztour"
+    custom_fields = [
+        "href",
+        "preview_img",
+        "location_name",
+        "hotel_id",
+        "hotel_rating",
+        "hotel_rating_text",
+        "latitude",
+        "longitude",
+        "title",
+        "hint_text",
+        "amenities_list",
+        "departure_info",
+        "mealplan",
+        "room_type",
+        "currency",
+        "price",
+        "price_box",
+        "price_include",
+        "till_info",
+        "stars_class",
+    ]
+
+
 def get_cards(soup: BeautifulSoup) -> List[dict]:
-    return soup.find_all(
-        "div", class_="hotel_point"
-    )
+    """functions to get hotel cards from soup, change it to match your case"""
+    return soup.find_all("div", class_="hotel_point")
+
 
 @parse_func_wrapper("https://tourist.tez-tour.com/")
 def parse_card(card: BeautifulSoup) -> dict:
-
+    """function to parse one hotel card, change it to match your case"""
     _, href = get_text(card, "a", class_="fav-detailurl", attrs=["href"])
-    
+
     _, preview_img = get_text(card, "img", class_="preview", attrs=["src"])
 
     try:
         (
-            location_name, hotel_id, hotel_rating, 
-            hotel_rating_text, latitude, longitude, title
+            location_name,
+            hotel_id,
+            hotel_rating,
+            hotel_rating_text,
+            latitude,
+            longitude,
+            title,
         ) = get_text(
-            card, "div", class_="city-name",
+            card,
+            "div",
+            class_="city-name",
             attrs=[
-                "data-hotel-id", "data-hotel-rating", "data-hotel-rating-text",
-                "data-lat", "data-lng", "data-title",
-            ]
+                "data-hotel-id",
+                "data-hotel-rating",
+                "data-hotel-rating-text",
+                "data-lat",
+                "data-lng",
+                "data-title",
+            ],
         )
     except:
         location_name = get_text(card, "div", class_="city-name")
@@ -112,43 +306,46 @@ def parse_card(card: BeautifulSoup) -> dict:
         latitude = None
         longitude = None
         title = None
-    
+
     _, hint_text = get_text(
-        card, "div", class_="clipped-text", attrs=["data-title"],
-        raise_error=False
+        card, "div", class_="clipped-text", attrs=["data-title"], raise_error=False
     )
 
-    amenities = card.find_all(
-        "h6", class_="hotel-amenities-item"
-    )
+    amenities = card.find_all("h6", class_="hotel-amenities-item")
 
-    amenities_list = ";".join(list(map(
-        lambda x: x.get_text(" ", strip=True),
-        amenities
-    )))
+    amenities_list = ";".join(
+        list(map(lambda x: x.get_text(" ", strip=True), amenities))
+    )
 
     departure_info = (
         card.find("div", class_="inline-visible")
         .find("div", class_="type")
-        .get_text(" ", strip=True))
-
-    mealplan = get_text(
-        card, "div", class_="fav-mealplan"
+        .get_text(" ", strip=True)
     )
 
-    room_type = get_text(
-        card, "div", class_="fav-room"
-    )
+    mealplan = get_text(card, "div", class_="fav-mealplan")
+
+    room_type = get_text(card, "div", class_="fav-room")
     _, currency, price = get_text(
-        card, "a", class_="price-box", attrs=["data-currency", "data-price", ]
+        card,
+        "a",
+        class_="price-box",
+        attrs=[
+            "data-currency",
+            "data-price",
+        ],
     )
 
     price_box = get_text(
-        card, "div", class_="price-box-hint",
+        card,
+        "div",
+        class_="price-box-hint",
     )
 
     price_include = get_text(
-        card, "ul", class_="price-include",
+        card,
+        "ul",
+        class_="price-include",
     )
 
     _, stars_class_list = get_text(card, "div", "hotel-star-box", attrs=["class"])
@@ -177,118 +374,28 @@ def parse_card(card: BeautifulSoup) -> dict:
         "price_box": price_box,
         "price_include": price_include,
         "till_info": till_info,
-        "stars_class": stars_class
+        "stars_class": stars_class,
     }
 
 
-def load_process_html_cards_from_s3(
-    client, Bucket: str, Key: str,
-    get_cards: Callable, parse_card: Callable
-) -> list:
-    prefix = "/".join(Key.split("/")[:-1])
-    object_key = os.path.join(prefix, "content.html")
-    meta_key = os.path.join(prefix, "meta.json")
-
-    meta_object_response =client.get_object(
-        Bucket=Bucket, Key=meta_key)
-
-    meta = json.loads(meta_object_response["Body"].read())
-
-    if not meta["failed"]:
-        get_object_response = client.get_object(
-            Bucket=Bucket, Key=object_key)
-        content = get_object_response["Body"].read()
-        soup = BeautifulSoup(content, "html.parser")
-        cards = get_cards(soup)
-        result = list(map(parse_card, cards))
-        result_with_meta = update_dicts(
-            result, parsing_id=meta["parsing_id"], key=Key, bucket=Bucket)
-        
-        client.delete_object(Bucket=Bucket, Key=Key)
-        client.delete_object(Bucket=Bucket, Key=object_key)
-        client.delete_object(Bucket=Bucket, Key=meta_key)
-        return result_with_meta
-    else:
-        client.delete_object(Bucket=Bucket, Key=Key)
-        client.delete_object(Bucket=Bucket, Key=meta_key)
-        return None
-
-def format_record(record: list):
-    template = (
-        '("{href}","{preview_img}","{location_name}",'
-        '"{hotel_id}","{hotel_rating}","{hotel_rating_text}",'
-        '"{latitude}","{longitude}","{title}","{hint_text}",'
-        '"{amenities_list}","{departure_info}","{mealplan}",'
-        '"{room_type}","{currency}","{price}","{price_box}",'
-        '"{price_include}","{till_info}","{stars_class}",'
-        'cast("{created_dttm}" as datetime), "{website}",'
-        '"{link}", "{offer_hash}", "{row_id}", "{parsing_id}",'
-        '"{key}", "{bucket}")'
+def handler(event, context):
+    s3_client = get_s3_client()
+    Bucket = event["messages"][0]["details"]["bucket_id"]
+    Key = event["messages"][0]["details"]["object_id"]
+    data = load_process_html_cards_from_s3(
+        s3_client, Bucket, Key, get_cards, parse_card
     )
-    return template.format(**record)
-
-def create_statement(data: list) -> str:
-    query = """
-    REPLACE INTO `parser/raw/teztour` (
-        href, preview_img, location_name, hotel_id, hotel_rating,
-        hotel_rating_text,latitude,longitude,title,hint_text,
-        amenities_list,departure_info,mealplan,room_type,currency,
-        price,price_box,price_include,till_info,stars_class,
-        created_dttm, website, link, offer_hash, row_id, parsing_id, 
-        key, bucket)
-    VALUES
-    {}
-    """.format(',\n'.join(map(format_record, data)))
-
-    return query
-
-def create_queries(data, max_records):
-
-    queries = []
-    for i in range(len(data) // max_records + 1):
-        sub_data = data[i * max_records: (i + 1) * max_records]
-        if len(sub_data) > 0:
-            queries.append(
-                create_statement(data[i * max_records: (i + 1) * max_records])
-            )
-    return queries
-
-def create_execute_query(query):
-  # Create the transaction and execute query.
-    def _execute_query(session):
-        session.transaction().execute(
-            query,
-            commit_tx=True,
-            settings=ydb.BaseRequestSettings().with_timeout(3).with_operation_timeout(2)
-        )
-    return _execute_query
-
-def process_file(Bucket, Key):
-    result = load_process_html_cards_from_s3(
-        s3, Bucket, Key, 
-        get_cards, parse_card
-    )
-
-    if result is None:
+    if data is None:
         return 0
 
-    queries = create_queries(result, max_records=3)
-    for query in queries:
-        try:
-            pool.retry_operation_sync(create_execute_query(query))
-        except Exception as e:
-            raise ValueError(f"Failed with exception {e} at query {query}")
-        
-    return len(result)
-    
-def handler(event, context):
+    length = len(data)
 
-    length = process_file(
-        Bucket=event["messages"][0]["details"]["bucket_id"],
-        Key=event["messages"][0]["details"]["object_id"]
-    )
+    session = initialize_session()
+
+    table = RawTeztour("parser", session, max_records=3)
+    table.bulk_insert(data)
 
     return {
         "objects": length,
-        'statusCode': 200,
+        "statusCode": 200,
     }
